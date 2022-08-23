31199f86ed337f810a93c69424ec2d5e22f20b01
boby <glboby27@gmail.com>
2022-08-23 16:11:11 +0900

    add audio channel log

--- js/receiver.js
+++ js/receiver.js
@@ -106,6 +106,10 @@ playerManager.addEventListener(
  */
 castDebugLogger.debug(LOG_RECEIVER_TAG , 'audio/mp4_mp4a.40.5 : ' + context.canDisplayType('audio/mp4', 'mp4a.40.5'));
 castDebugLogger.debug(LOG_RECEIVER_TAG , 'audio/mp4_mp4a.40.2 : ' + context.canDisplayType('audio/mp4', 'mp4a.40.2'));
+castDebugLogger.debug(LOG_RECEIVER_TAG , 'audio/mp4_mp4a.40.5 - 2channel : ' + context.canDisplayType('audio/mp4', 'mp4a.40.5', 2));
+castDebugLogger.debug(LOG_RECEIVER_TAG , 'audio/mp4_mp4a.40.2 - 2channel : ' + context.canDisplayType('audio/mp4', 'mp4a.40.2', 2));
+castDebugLogger.debug(LOG_RECEIVER_TAG , 'audio/mp4_mp4a.40.5 - 5.1channel : ' + context.canDisplayType('audio/mp4', 'mp4a.40.5', 6));
+castDebugLogger.debug(LOG_RECEIVER_TAG , 'audio/mp4_mp4a.40.2 - 5.1channel : ' + context.canDisplayType('audio/mp4', 'mp4a.40.2', 6));
 castDebugLogger.debug(LOG_RECEIVER_TAG , 'video/mp4_avc1.4d4028 : ' + context.canDisplayType('video/mp4', 'avc1.4d4028'));
 castDebugLogger.debug(LOG_RECEIVER_TAG , 'video/mp4_avc1.64001e : ' + context.canDisplayType('video/mp4', 'avc1.64001e'));
 
